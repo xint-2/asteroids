cf110887808abb6954da8b7a405fd7dcf6ae847a
xint-2 <tjmarra77@gmail.com>
2025-01-27 20:21:48 -0500

    asteroids in

---
+++ asteroid.py
@@ -0,0 +1,14 @@
+import pygame
+from circleshape import CircleShape
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white",(self.position.x, self.position.y), self.radius, 2)
+    
+    def update(self, dt):
+        # sub-classes must override
+        self.position += (self.velocity * dt) 
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -9,12 +12,34 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    entity = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
+
     while True:
-        screen.fill(000)
-        pygame.display.flip
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        screen.fill("black")
+
+        for entity in updatable:
+            entity.update(dt)
+        for entity in drawable:
+            entity.draw(screen)
+
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000 
 
 
 if __name__ == "__main__":
